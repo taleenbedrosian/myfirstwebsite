356701597a2c81477bbcb442258ea79a7d86aa40
Taleen Bedrosian <taleen.bedrosian@gmail.com>
2023-10-03 13:22:47 -0400

    changed

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${3000}`);
+});
+app.use('/views/images/', express.static('./views/images'));
